713633e55b2214167a5d62306fc5683a1acc7db7
ld8 <don_lee@me.com>
2020-05-05 18:57:51 +0800

    Updates

---
+++ precache-manifest.c16f6f82fbe35ea218e103fd249ffaba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dd9737ee743286067051af9b2d03d5c",
+    "url": "/react-budget-app/index.html"
+  },
+  {
+    "revision": "f2c05a4afe7a27a5e23f",
+    "url": "/react-budget-app/static/css/main.248d7ac6.chunk.css"
+  },
+  {
+    "revision": "4787da21896d43ddab39",
+    "url": "/react-budget-app/static/js/2.f7086f4a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-budget-app/static/js/2.f7086f4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2c05a4afe7a27a5e23f",
+    "url": "/react-budget-app/static/js/main.3bb3d032.chunk.js"
+  },
+  {
+    "revision": "9aaf3089d0e35eb32c41",
+    "url": "/react-budget-app/static/js/runtime-main.70b6f1d6.js"
+  }
+]);
